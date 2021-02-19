26be48f8957a32244a1bb4d204e1bb2dd899a34d
jasscia <1648511816@qq.com>
2021-02-19 21:55:02 +0800

    995_2

--- 995_minKBitFlips.js
+++ 995_minKBitFlips.js
@@ -5,20 +5,34 @@
  */
 var minKBitFlips = function(A, K) {
     let count = 0
-    for(let i =0;i<=A.length - K;i++) {
-      if(A[i]===0) {
-        for(let j=i;j<i+K;j++){
-          A[j] = A[j] ? 0 : 1
-        }
+    let array=[];
+    let sum=0
+    let i=0
+    for(;i<=A.length-K;i++) {
+      if(((sum+A[i])&1)===0) {
+        array.push(1)
         count++
+        sum+=1
+      } else {
+        array.push(0)
+      }
+      if(i>=K-1) {
+        sum -= array.shift()||0
       }
     }
-    if(A.includes(0)) {
-      return -1
+    //console.log(i,array,sum,count)
+    for(;i<A.length;i++){
+      if(((sum+A[i])&1)===0) {
+        return -1
+      } else {
+        if(A.length-i<=array.length){
+          sum-=array.shift()||0
+        }
+      }
     }
     return count
 };
 
-[[[0,1,0],1],[[0,1,0],2],[[1,1,0],1],[[1,1,0],2],[[0,0,0,1,0,1,1,0],3]].forEach(([A,K])=>{
+[[[0,0,1],3],[[0,1,0],2],[[1,1,0],1],[[1,1,0],2],[[0,0,0,1,0,1,1,0],3]].slice(1,10).forEach(([A,K])=>{
   console.log(minKBitFlips(A,K))
 })
