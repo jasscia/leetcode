cff2d3f254b1c33b63ffddcd10dbda2fbf6af029
jasscia <1648511816@qq.com>
2021-02-23 22:02:32 +0800

    1052

---
+++ 1052_maxSatisfied.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} customers
+ * @param {number[]} grumpy
+ * @param {number} X
+ * @return {number}
+ */
+var maxSatisfied = function(customers, grumpy, X) {
+  let array=[]
+  let res=0
+  for(let i=0;i<grumpy.length;i++){
+    if(grumpy[i]) {
+      array.push(customers[i])
+    } else {
+      array.push(0)
+      res+=customers[i]
+    }
+  }
+  let max=0
+  let sum=0
+  for(let i=0;i<array.length;i++){
+    if(i>=X){
+      sum-=array[i-X]
+    }
+    sum+=array[i]
+    max=Math.max(max, sum)
+    console.log(i,sum)
+  }
+  console.log(array, max, res)
+  return res+max
+};
+[[[1,0,1,2,1,1,7,5],
+[0,1,0,1,0,1,0,1],
+3],[[3,2,5],
+[0,1,1],
+2]].slice(1,2).forEach(([customers,grumpy,X]) => {
+  console.log(maxSatisfied(customers,grumpy,X))
+})
