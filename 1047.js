9b0a3e5a7116826618f1d60377713f56c12137ef
jasscia <1648511816@qq.com>
2021-03-09 23:48:32 +0800

    1047

---
+++ 1047.js
@@ -0,0 +1,14 @@
+function  removeDuplicates(S){
+  const stack=[]
+  function getTop(){
+    return stack[stack.length-1]
+  }
+  for(let i=0;i<S.length;i++){
+    if(stack.length && getTop()===S[i]){
+      stack.pop()
+    } else {
+      stack.push(S[i])
+    }
+  }
+  return stack.join('')
+}
