ad64ed4f45c31604d0b893a6af9579406e5c6bfc
jasscia <1648511816@qq.com>
2021-03-15 00:09:12 +0800

    705

--- 705.js
+++ 705.js
@@ -2,19 +2,31 @@
  * Initialize your data structure here.
  */
 var MyHashSet = function() {
-  this.hash = {}
+  this.base = 769
+  this.data = Array(769)
+  this.hash = function(v){
+    return v % this.base
+  }
 
 };
 
 MyHashSet.prototype.add = function(key) {
-  this.hash[key] = this.hash[key] ||[]
-  this.hash[key].push(key)
+  const h = this.hash(key)
+  const e = this.data.find((_,index)=>index===t)
+  if(e){
+    e.push(h)
+  } else {
+    this.data[h]=[h]
+  }
 };
 
 MyHashSet.prototype.remove = function(key) {
-  delete this.hash[key]
+  const h = this.hash(key)
+  delete this.data[h]
 };
 
 MyHashSet.prototype.contains = function(key) {
-  return !!this.hash[key]
+  const h = this.hash(key)
+  const e = this.data.find((_,index)=>index===t)
+  return !!e
 };
