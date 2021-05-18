7c89d7fa144c2849e12e043a32d39a2bf060faf1
jasscia <1648511816@qq.com>
2021-05-18 23:52:16 +0800

    -

---
+++ 993_isCousins2.js
@@ -0,0 +1,29 @@
+function isCousins (root, x, y){
+  let xIndex=-1;yIndex=-1
+  for(let i=0;i<root.length;i++){
+    if(~xIndex && ~yIndex){
+      return
+    }
+    if(root[i]===x){
+      xIndex=findParent(i)
+    }
+    if(root[i]===y){
+      yIndex=findParent(i)
+    }
+  }
+  if(xIndex ===yIndex){
+    return false
+  }
+  while(xIndex>0 && yIndex>0){
+    xIndex  = findParent(xIndex)
+    yIndex  = findParent(yIndex)
+  }
+  return xIndex===yIndex
+  function findParent(index){
+    if(index&1){
+      return (index-1)/2
+    } else {
+      return (index-2)/2
+    }
+  }
+}
